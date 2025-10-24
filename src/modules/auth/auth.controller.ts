d17925d073db3a49116cee93e92efe9daed1ec22
Esraa <esraareda17@gmail.com>
2025-10-24 18:59:31 +0300

    ...

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -5,13 +5,19 @@ import {
   HttpStatus,
   Patch,
   Post,
+  Req,
+  Res,
 } from '@nestjs/common';
 import { AuthenticationService } from './auth.service';
 import {
   ConfirmEmailDto,
+  ForgotPasswordCodeDto,
+  GmailAuthDto,
   LoginBodyDto,
   ResendConfirmEmailDto,
+  ResetForgotPasswordDto,
   SignupBodyDto,
+  VerifyForgotPasswordDto,
 } from './dto/signup.dto';
 import { LoginCredentialsResponse } from 'src/common';
 import { LoginResponse } from './entities/auth.entity';
@@ -65,4 +71,59 @@ export class AuthenticationController {
     const credentials = await this.authenticationService.login(body);
     return { message: 'Done', data: { credentials } };
   }
+
+  @Post('forgot-password')
+  @HttpCode(HttpStatus.OK)
+  async forgotPassword(
+    @Body()
+    body: ForgotPasswordCodeDto,
+  ): Promise<{ message: string }> {
+    await this.authenticationService.sendForgotPasswordCode(body); 
+    return { message: 'Password reset instructions sent to your email, if the user exists.' };
+  }
+
+  @Post('verify-otp')
+  @HttpCode(HttpStatus.OK)
+  async verifyPasswordResetOtp(
+    @Body()
+    body: VerifyForgotPasswordDto,
+  ): Promise<{ message: string }> {
+    await this.authenticationService.verifyForgotPassword(body);
+    return { message: 'OTP verified successfully.' };
+  }
+
+  @Patch('reset-password')
+  async resetPassword(
+    @Body()
+    body: ResetForgotPasswordDto,
+  ): Promise<{ message: string }> {
+    await this.authenticationService.resetForgotPassword(body);
+    return { message: 'Password has been updated successfully.' };
+  }
+
+  @Post('gmail/signup')
+  async signupWithGmail(@Body() body: GmailAuthDto, @Req() req: Request, @Res({ passthrough: true }) res: Response) {
+    const credentials = await this.authenticationService.signupWithGmail(body);
+
+    return { 
+        statusCode: 201, 
+        message: 'Signup successful', 
+        data: { credentials } 
+    };
+  }
+
+  @Post('gmail/login')
+  @HttpCode(HttpStatus.OK) 
+  async loginWithGmail(@Body() body: GmailAuthDto) {
+    const credentials = await this.authenticationService.loginWithGmail(body);
+    
+    return { 
+        statusCode: 200, 
+        message: 'Login successful', 
+        data: { credentials } 
+    };
+  }
 }
+
+
+
